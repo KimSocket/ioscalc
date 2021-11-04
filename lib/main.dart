e7911018f5a174ec61c49d97c87dd587d257a58a
KimSocket <socket.ryooma@gmail.com>
2021-11-04 22:14:53 +0900

    fyi

--- lib/main.dart
+++ lib/main.dart
@@ -30,29 +30,304 @@ class _CalcmainState extends State<Calcmain> {
 
       appBar: null,
       body: Center(
-       child: Row(
+       child: Column(
          children: <Widget>[
-           SizedBox(),
-           SizedBox(),
-           SizedBox(),
-           Column(
-               children: <Widget>[]
-
+           SizedBox(
+             height: 100,
+             width: 10,
+           ),
+           SizedBox(
+             height: 100,
+           ),//결과창 표시 예정
+           SizedBox(
+             height: 100,
+             width: 10,
+           ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceAround,
+               children: <Widget>[
+                 Container(
+                   height: 60,
+                   width: 60,
+                   color: Colors.white70,
+                    child:InkWell(
+                      highlightColor: Colors.white,
+                      child: Text('AC',style: TextStyle(fontSize: 40)),
+                      onTap: (){
+                        setState(() {
+                        });
+                    },
+                    ),
+                 ),
+                 Container(
+                   height: 60,
+                   width: 60,
+                   color: Colors.white70,
+                   child:InkWell(
+                     highlightColor: Colors.white,
+                     child: Text('+/-',style: TextStyle(fontSize: 40)),
+                     onTap: (){
+                       setState(() {
+                       });
+                     },
+                   ),
+                 ),
+                 Container(
+                   height: 60,
+                   width: 60,
+                   color: Colors.white70,
+                   child:InkWell(
+                     highlightColor: Colors.white,
+                     child: Text('%',style: TextStyle(fontSize: 55)),
+                     onTap: (){
+                       setState(() {
+                       });
+                     },
+                   ),
+                 ),
+                 Container(
+                   height: 60,
+                   width: 60,
+                   color: Colors.orangeAccent,
+                   child:InkWell(
+                     highlightColor: Colors.white,
+                     child: Text('÷',style: TextStyle(fontSize: 50)),
+                     onTap: (){
+                       setState(() {
+                       });
+                     },
+                   ),
+                 ),
+               ],
            ),//ac,+/-,%,÷
-           Column(
-               children: <Widget>[]
-           ),//7,8,9,x
-           Column(
-               children: <Widget>[]
+           SizedBox(
+               height: 20,
+             ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceAround,
+             children: <Widget>[
+               Container(
+                 height: 60,
+                 width: 60,
+                 color: Colors.white10,
+                 child:InkWell(
+                   highlightColor: Colors.white,
+                   child: Text('7',style: TextStyle(fontSize: 50)),
+                   onTap: (){
+                     setState(() {
+                     });
+                   },
+                 ),
+               ),
+               Container(
+                 height: 60,
+                 width: 60,
+                 color: Colors.white10,
+                 child:InkWell(
+                   highlightColor: Colors.white,
+                   child: Text('8',style: TextStyle(fontSize: 50)),
+                   onTap: (){
+                     setState(() {
+                     });
+                   },
+                 ),
+               ),
+               Container(
+                 height: 60,
+                 width: 60,
+                 color: Colors.white10,
+                 child:InkWell(
+                   highlightColor: Colors.white,
+                   child: Text('9',style: TextStyle(fontSize: 50)),
+                   onTap: (){
+                     setState(() {
+                     });
+                   },
+                 ),
+               ),
+               Container(
+                 height: 60,
+                 width: 60,
+                 color: Colors.orangeAccent,
+                 child:InkWell(
+                   highlightColor: Colors.white,
+                   child: Text('×',style: TextStyle(fontSize: 60)),
+                   onTap: (){
+                     setState(() {
+                     });
+                   },
+                 ),
+               ),
+             ],
+             ),//7,8,9,x
+           SizedBox(
+             height: 20,
+           ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceAround,
+             children: <Widget>[
+               Container(
+                 height: 60,
+                 width: 60,
+                 color: Colors.white10,
+                 child:InkWell(
+                   highlightColor: Colors.white,
+                   child: Text('4',style: TextStyle(fontSize: 50)),
+                   onTap: (){
+                     setState(() {
+                     });
+                   },
+                 ),
+               ),
+               Container(
+                 height: 60,
+                 width: 60,
+                 color: Colors.white10,
+                 child:InkWell(
+                   highlightColor: Colors.white,
+                   child: Text('5',style: TextStyle(fontSize: 50)),
+                   onTap: (){
+                     setState(() {
+                     });
+                   },
+                 ),
+               ),
+               Container(
+                 height: 60,
+                 width: 60,
+                 color: Colors.white10,
+                 child:InkWell(
+                   highlightColor: Colors.white,
+                   child: Text('6',style: TextStyle(fontSize: 50)),
+                   onTap: (){
+                     setState(() {
+                     });
+                   },
+                 ),
+               ),
+               Container(
+                 height: 60,
+                 width: 60,
+                 color: Colors.orangeAccent,
+                 child:InkWell(
+                   highlightColor: Colors.white,
+                   child: Text('-',style: TextStyle(fontSize: 60)),
+                   onTap: (){
+                     setState(() {
+                     });
+                   },
+                 ),
+               ),
+             ],
            ),//4,5,6,-
-           Column(
-               children: <Widget>[]
+           SizedBox(
+             height: 20,
+           ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceAround,
+             children: <Widget>[
+               Container(
+                 height: 60,
+                 width: 60,
+                 color: Colors.white10,
+                 child:InkWell(
+                   highlightColor: Colors.white,
+                   child: Text('1',style: TextStyle(fontSize: 50)),
+                   onTap: (){
+                     setState(() {
+                     });
+                   },
+                 ),
+               ),
+               Container(
+                 height: 60,
+                 width: 60,
+                 color: Colors.white10,
+                 child:InkWell(
+                   highlightColor: Colors.white,
+                   child: Text('2',style: TextStyle(fontSize: 50)),
+                   onTap: (){
+                     setState(() {
+                     });
+                   },
+                 ),
+               ),
+               Container(
+                 height: 60,
+                 width: 60,
+                 color: Colors.white10,
+                 child:InkWell(
+                   highlightColor: Colors.white,
+                   child: Text('3',style: TextStyle(fontSize: 50)),
+                   onTap: (){
+                     setState(() {
+                     });
+                   },
+                 ),
+               ),
+               Container(
+                 height: 60,
+                 width: 60,
+                 color: Colors.orangeAccent,
+                 child:InkWell(
+                   highlightColor: Colors.white,
+                   child: Text('+',style: TextStyle(fontSize: 60)),
+                   onTap: (){
+                     setState(() {
+                     });
+                   },
+                 ),
+               ),
+             ],
            ),//1,2,3,+
-           Column(
-               children: <Widget>[]
+           SizedBox(
+             height: 20,
+           ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceAround,
+             children: <Widget>[
+               Container(
+                 height: 60,
+                 width: 60,
+                 color: Colors.white10,
+                 child:InkWell(
+                   highlightColor: Colors.white,
+                   child: Text('0',style: TextStyle(fontSize: 50)),
+                   onTap: (){
+                     setState(() {
+                     });
+                   },
+                 ),
+               ),
+
+               Container(
+                 height: 60,
+                 width: 60,
+                 color: Colors.white10,
+                 child:InkWell(
+                   highlightColor: Colors.white,
+                   child: Text('.',style: TextStyle(fontSize: 50)),
+                   onTap: (){
+                     setState(() {
+                     });
+                   },
+                 ),
+               ),
+               Container(
+                 height: 60,
+                 width: 60,
+                 color: Colors.orangeAccent,
+                 child:InkWell(
+                   highlightColor: Colors.white,
+                   child: Text('=',style: TextStyle(fontSize: 60)),
+                   onTap: (){
+                     setState(() {
+                     });
+                   },
+                 ),
+               ),
+             ],
            ), //0,.,=
-
-
          ],
        ),
       ),
